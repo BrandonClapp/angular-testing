5e524726f82a59a208bfd1efcc5bad0746c6e83a
Brandon Clapp <brandonclapp@outlook.com>
2023-04-30 18:26:51 -0500

    Observable notes

--- src/app/observables/observables.component.spec.ts
+++ src/app/observables/observables.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ObservablesComponent } from './observables.component';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 describe('ObservablesComponent', () => {
   let component: ObservablesComponent;
@@ -18,5 +19,134 @@ describe('ObservablesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('a ReplaySubject can still be subscribed to after it has been completed', (done) => {
+    const replaySubject = new ReplaySubject<string>(1);
+    replaySubject.next('Hello');
+    replaySubject.complete();
+
+    replaySubject.subscribe((value) => {
+      expect(value).toBe('Hello');
+      done();
+    });
+  });
+
+  it('adding new values to a ReplaySubject after it has been completed will not emit the new values', (done) => {
+    const replaySubject = new ReplaySubject<string>(1);
+    replaySubject.next('Hello');
+    replaySubject.complete();
+    replaySubject.next('World');
+
+    let count = 0;
+
+    replaySubject.subscribe((value) => {
+      count++;
+    });
+
+    // Should only have one value
+    expect(count).toBe(1);
+    done();
+  });
+
+  // Question: Does a ReplaySubject emit all stored buffer values to new subscribers if it's already completed?
+  // Answer: Yes
+  it('a ReplaySubject will replay the entire buffer to new subscribers after completion', (done) => {
+    const replaySubject = new ReplaySubject<string>(2);
+    replaySubject.next('Hello');
+    replaySubject.next('World');
+
+    // Test will pass regardless of whether the ReplaySubject is completed or not
+    // since all values are emitted upon subscription both before and after the ReplaySubject is completed
+    replaySubject.complete();
+
+    let count = 0;
+
+    replaySubject.subscribe((value) => {
+      count++;
+
+      if (count === 2) {
+        // In a ReplaySubject, all buffer values are emitted upon subscription even if the ReplaySubject is completed
+        // This is different from a BehaviorSubject, which the single value in the buffer is not emitted.
+        // Subscribers to a completed BehaviorSubject are not called.
+
+        // Ensure that both values in the buffer have been emitted,
+        // eventhough the ReplaySubject is completed
+        expect(count).toEqual(2);
+        done();
+      }
+    });
+  });
+
+  it('a BehaviorSubject will emit the last value upon subscription', (done) => {
+    const behaviorSubject = new BehaviorSubject<string>('Hello');
+    behaviorSubject.next('World');
+
+    // The subscriber will receive the last value emitted from the BehaviorSubject
+    behaviorSubject.subscribe((value) => {
+      expect(value).toEqual('World');
+      done();
+    });
+  });
+
+  it('a BehaviorSubject will not emit any values after it has been completed', (done) => {
+    const behaviorSubject = new BehaviorSubject<string>('Hello');
+    behaviorSubject.next('World');
+    behaviorSubject.complete();
+
+    // This subscriber will not receive any values because the BehaviorSubject is completed
+    // If a late subscriber tries to subscribe to a completed BehaviorSubject, it will immediately
+    // receive a complete notification without receiving any values.
+    behaviorSubject.subscribe((value) => {
+      // This callback is not evaluated because the BehaviorSubject is completed
+      // If this line was hit, it would cause the test to fail
+      // If you subscribe to a completed BehaviorSubject, you won’t receive the last value but for a ReplaySubject(1) you will receive the last value
+      // https://stackoverflow.com/questions/43118769/subject-vs-behaviorsubject-vs-replaysubject-in-angular
+      expect(false).toBeTruthy();
+    });
+
+    done();
+  });
+
+  it('Subscribers to a Subject only receives future emissions, as there is no buffer', (done) => {
+    const subject = new Subject<string>();
+    subject.next('I get dropped because I happen before all subscriptions');
+
+    subject.subscribe((value) => {
+      expect(value).toEqual('forty-two');
+      done();
+    });
+
+    subject.next('forty-two');
+  });
+
+  it('A Subject does not emit new values after completion', (done) => {
+    const subject = new Subject<string>();
+    subject.next('I get dropped because I happen before all subscriptions');
+
+    subject.subscribe((value) => {
+      expect(value).toEqual('forty-two');
+      expect(value).not.toEqual('forty-three');
+
+      // Hack to ensure that the test waits for all values to be emitted
+      setTimeout(() => {
+        done();
+      }, 1000);
+    });
+
+    subject.next('forty-two');
+    subject.complete();
+    subject.next('forty-three');
+  });
+
+  it('A subject does not emit null as first value', (done) => {
+    const subject = new Subject<string>();
+
+    subject.subscribe((value) => {
+      expect(value).not.toEqual(null);
+      done();
+    });
+
+    subject.next('forty-two');
   });
 });
--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  ReplaySubject,
+  Subject,
+  scan,
+} from 'rxjs';
 
 /**
  * Experimenting with observable types
@@ -22,21 +28,36 @@ export class ObservablesComponent implements OnInit {
   );
 
   // Subject is an abstraction of Observable that allows values to be multicasted to many Observers.
-  public buttonClicked$: Subject<string> = new Subject<string>();
+  // A Subject does not have an initial value and subscribers will not receive a value until one is emitted.
+  // It does not emit null, only future emissions.
+  public buttonClicked$: Subject<boolean> = new Subject<boolean>();
 
-  // BehaviorSubject is a type of Subject that requires an initial value and emits its current value whenever it is subscribed to.
+  // BehaviorSubject is a type of Subject that requires an initial value and emits its current value whenever
+  // it is subscribed to. Because of this, it supports "late subscribers", or subscribers that subscribe after
+  // the BehaviorSubject has emitted a value.
+  // While the BehaviorSubject will emit it's value to late subscribers, it will not emit to late subscribers if it is already completed.
+  // See: https://stackoverflow.com/questions/43118769/subject-vs-behaviorsubject-vs-replaysubject-in-angular
+  // See test: 'a BehaviorSubject will not emit any values after it has been completed'
   public showSupportButton$: BehaviorSubject<boolean> =
     new BehaviorSubject<boolean>(false);
 
   // ReplaySubject is a type of Subject that records multiple values from its observable and replays them to new subscribers.
-  // For example, if you want to get the last 2 values of an observable, you can use ReplaySubject(2).
-  public messages$ = new ReplaySubject<string>(4);
+  // The ReplaySubject emits each stored value to new subscribers, even those from before the subscription started.
+  // Note that this does not mean it's an array of 5 values, it means it will replay the last 5 values.
+  messages$: ReplaySubject<string> = new ReplaySubject<string>(5);
+
+  // This is subscribed to from the template using the async pipe
+  // Aggregates each message emmited from the ReplaySubject into an array
+  messagesArray$: Observable<string[]> = this.messages$.pipe(
+    // keep an array of all emitted values in the ReplaySubject
+    scan((acc: string[], value: string) => [...acc, value], [])
+  );
 
   clickButton(event: any): void {
-    this.buttonClicked$.next('Clicked!');
+    this.buttonClicked$.next(true);
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     // With a subject, our view dosen't know about this value because the subscription
     // happens after the value is emitted. This is why we use a BehaviorSubject instead,
     // which will emit the value to the view when it is subscribed to.
@@ -45,11 +66,17 @@ export class ObservablesComponent implements OnInit {
     // Change the originally false emmission from declaration to true
     this.showSupportButton$.next(true);
 
-    // TODO: Figure this out. Still don't understand how ReplaySubects cache individual values
-    // and them are able to emit them all at once into an array on the template?...
+    // Add initial messages to the ReplaySubject buffer
     this.messages$.next('Message 1');
     this.messages$.next('Message 2');
     this.messages$.next('Message 3');
     this.messages$.next('Message 4');
+    this.messages$.next('Message 5');
+
+    // Add new messages to the ReplaySubject and update the view
+    setInterval(() => {
+      const newMessage = `Message ${Math.floor(Math.random() * 100)}`;
+      this.messages$.next(newMessage);
+    }, 1000);
   }
 }
